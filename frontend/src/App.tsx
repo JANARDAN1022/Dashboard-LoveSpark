896a04ec7bddfa9025a738b90c0eae8b759944ca
JD <janardanayachi3@gmail.com>
2023-08-10 22:29:56 +0530

    Deployment Changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter as Router,Routes,Route, useNavigate } from "react-router-dom";
 import Mainpage from "./Components/Pages/MainPage";
 import Login from "./Components/Layout/Login";
 import Register from "./Components/Layout/Register";
 import UpdateInfo from "./Components/Layout/UpdateInfo";
+import { useAppSelector } from "./Hooks";
 
 
 function App() {  
+const Navigate = useNavigate();
+const {Admin} = useAppSelector((state)=>state.user);
 
+
+useEffect(()=>{
+if(!Admin){
+  Navigate('/Login');
+}
+},[Admin]);
 
 
   return (

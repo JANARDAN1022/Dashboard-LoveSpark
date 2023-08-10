0fba5b9a2e6725259471e26c2c764da0a416b8e4
JD <janardanayachi3@gmail.com>
2023-08-10 22:50:01 +0530

    adding loaders

--- frontend/src/Components/Layout/NavBar.tsx
+++ frontend/src/Components/Layout/NavBar.tsx
@@ -2,7 +2,6 @@ import {useContext,useState} from 'react';
 import {RxHamburgerMenu} from 'react-icons/rx';
 import { MainPageContext } from '../../Context/MainPageContext';
 import {RiAdminLine} from 'react-icons/ri';
-//import {AiOutlineSearch} from 'react-icons/ai';
 import {IoIosNotificationsOutline} from 'react-icons/io';
 import {BiLogOut} from 'react-icons/bi';
 import { useAppSelector,useAppDispatch } from '../../Hooks';
@@ -17,29 +16,29 @@ interface NavbarProps {
 
 const NavBar = ({ToggleBar,setToggleBar}:NavbarProps) => {
     const {ShowComponent} = useContext(MainPageContext);
-   // const [Search,setSearch]=useState(false);
-   // const SearchRef = useRef<HTMLInputElement>(null);
     const [Tooltip,setTooltip]=useState(false);
     const [ShowAcccount,setShowAcccount]=useState(false);
+    const [Loading,setLoading]=useState(false);
     const {Admin} = useAppSelector((state)=>state.user);
     const dispatch = useAppDispatch();
     const Navigate = useNavigate();
 
 
     const HandleLogout = async()=>{
+      if(!Loading){
+      setLoading(true);
      const response = await dispatch(LogoutUser());
-      if (response.meta.requestStatus === 'fulfilled') {
+      if (response.meta.requestStatus === 'fulfilled'){
+        setLoading(false);
        Navigate('/Login');
+      }else{
+        setLoading(false);
       }
+    }else{
+      return;
+    }
     }
    
-  /*  const HandleSearch = ()=>{
-        setSearch(!Search);
-        if(SearchRef.current){
-            SearchRef.current.focus();
-        }
-    }*/
-
   return (
     <div className={`${ShowComponent!=='Reports'?'h-[165px]':'h-[81px]'} w-full  flex flex-col  border-b border-[rgba(255,255,255,0.8)] bg-gradient-to-r from-gray-900 via-gray-900 to-black`}>
     <div className='flex justify-between mt-5'>
@@ -80,6 +79,12 @@ const NavBar = ({ToggleBar,setToggleBar}:NavbarProps) => {
                     }, 500);
                     }} onMouseLeave={()=>setTooltip(false)} size={28} className='text-white cursor-pointer'/>
                 <span className={`${Tooltip?'':'hidden'} top-[-30px] text-white absolute`}>Logout</span>
+                <div className={`${Loading?'':'hidden'}  top-[-30px] absolute`}>
+                <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                    </svg>
+         </div>
             </div>
         </div>
     </div>
